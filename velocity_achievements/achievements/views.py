f4b6b0ed18777bf2f1e4241ded8b9c609c20e104
Christophe Biocca <christophe.biocca@gmail.com>
2012-01-06 00:46:50 +0100

    Started including the Request context, so that we can track user loginness everywhere

--- velocity_achievements/achievements/views.py
+++ velocity_achievements/achievements/views.py
@@ -1,7 +1,8 @@
 from django.db.models import Count
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from models import Participant
 
 def achievements(request):
     participants = Participant.objects.annotate(num_grants=Count('grant')).filter(num_grants__gt = 0).order_by('-num_grants')
-    return render_to_response('achievements.html', {'participants': participants})
+    return render_to_response('achievements.html', {'participants': participants}, context_instance=RequestContext(request))
