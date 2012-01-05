eabf030b8abe79af3dc29d05c2ec75f289e884a7
Christophe Biocca <christophe.biocca@gmail.com>
2012-01-06 00:56:20 +0100

    Stopped redirecting to dispatch, included support for signup view.

--- velocity_achievements/emailauth/views.py
+++ velocity_achievements/emailauth/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render_to_response, redirect
 from django.contrib.auth import login as auth_login, authenticate as auth_authenticate
 from django.http import Http404
 from django.template.context import RequestContext
-from forms import PasswordInitialSetForm
+from forms import PasswordInitialSetForm, SignupForm
+from models import UserWithEmail
 
 def initial_password_set(request, token, template_name='emailauth/set_password.html'):
     if request.method == 'POST':
@@ -12,16 +13,19 @@ def initial_password_set(request, token, template_name='emailauth/set_password.h
             user = auth_authenticate(email = user.primary_email.email, password = form.cleaned_data['password'])
             print ("user backend %s" % user.backend)
             auth_login(request, user)
-            return redirect(dispatch)
+            return redirect('achievements')
     elif token:
         form = PasswordInitialSetForm(initial = {'token': token})
     else: 
         raise Http404('Need token')
     return render_to_response(template_name, {'form': form, 'token':token}, context_instance=RequestContext(request))
-    
-def dispatch(request):
-    user = request.user
-    if user.is_authenticated():
-        return redirect(user.next_step())
+
+def signup(request, template_name='emailauth/signup.html', user_class=UserWithEmail):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user_class.objects.create_user(**form.cleaned_data)
+            redirect('will_notify')
     else:
-        return redirect('login')
+        form = SignupForm()
+    return render_to_response(template_name, {'form': form}, context_instance=RequestContext(request))
