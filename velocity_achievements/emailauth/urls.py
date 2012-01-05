30f6541a0a042ea8dc693396f75766628eb24349
Christophe Biocca <christophe.biocca@gmail.com>
2012-01-06 00:57:00 +0100

    Fixed up auth urls

--- velocity_achievements/emailauth/urls.py
+++ velocity_achievements/emailauth/urls.py
@@ -1,16 +1,13 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 import forms
 import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns('',
-    url(r'^login/$', auth_views.login, name="customerlogin", kwargs={'authentication_form': forms.AuthenticationForm}),
-    url(r'^logout/$', auth_views.logout_then_login, name="customerlogout", kwargs={'login_url': 'customerlogin'}),
+    url(r'^login/$', auth_views.login, name="login", kwargs={'authentication_form': forms.AuthenticationForm, 'template_name': 'emailauth/login.html'}),
+    url(r'^logout/$', auth_views.logout_then_login, name="logout", kwargs={'login_url': '/login'}),
     url(r'^activate/(?P<token>[0-9A-Za-z]{40})/', views.initial_password_set, name='activate'),
-    url(r'^password_change/$', auth_views.password_change, name = 'change_password', kwargs={"post_change_redirect": 'useraccount'}),
-#    url(r'^password_reset/$', 'django.contrib.auth.views.password_reset'),
-#    url(r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
-#    url(r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 'django.contrib.auth.views.password_reset_confirm'),
-#    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
-    url(r'^dispatch/$', views.dispatch, name="dispatch")
+    url(r'^password_change/$', auth_views.password_change, name = 'change_password', kwargs={"post_change_redirect": 'achievements'}),
+    url(r'^signed_up/$', direct_to_template, name='will_notify', kwargs={'template': 'emailauth/will_notify.html'}),
 )
