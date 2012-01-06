66650a1da924f283fe218374c22b72c1998bd817
Christophe Biocca <christophe.biocca@gmail.com>
2012-01-06 01:01:39 +0100

    added signup link

--- velocity_achievements/achievements/urls.py
+++ velocity_achievements/achievements/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
 from views import achievements
+from models import Participant
+from emailauth.views import signup
 
 urlpatterns = patterns('',
     url(r'^$', achievements, name='achievements'),
+    url(r'^signup/$', signup, name='signup', kwargs={'user_class': Participant})
 )
