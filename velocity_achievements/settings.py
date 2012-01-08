0d2fc01e3be5b15145bdc61dd9ea477675835f77
Christophe Biocca <christophe.biocca@gmail.com>
2012-01-08 01:14:48 -0500

    Made GLaDOS@velocitythegame.com the default from email

--- velocity_achievements/settings.py
+++ velocity_achievements/settings.py
@@ -45,6 +45,8 @@ STATIC_URL = '/static/'
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
+DEFAULT_FROM_EMAIL = 'GLaDOS@velocitythegame.com'
+
 # Additional locations of static files
 STATICFILES_DIRS = (
     os.path.join(ROOT_PATH, "static"),
