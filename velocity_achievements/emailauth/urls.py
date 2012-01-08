3a409a8be0eb0feaa0e88f5450ff4ed79c55e1e5
Christophe Biocca <christophe.biocca@gmail.com>
2012-01-08 01:13:18 -0500

    made the logout link not redirect to login. Also made it support a next-page link.

--- velocity_achievements/emailauth/urls.py
+++ velocity_achievements/emailauth/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns('',
     url(r'^login/$', auth_views.login, name="login", kwargs={'authentication_form': forms.AuthenticationForm, 'template_name': 'emailauth/login.html'}),
-    url(r'^logout/$', auth_views.logout_then_login, name="logout", kwargs={'login_url': '/login'}),
+    url(r'^logout/(?:(?P<next_page>.*)/)?$', auth_views.logout, name="logout"),
     url(r'^activate/(?P<token>[0-9A-Za-z]{40})/', views.initial_password_set, name='activate'),
     url(r'^password_change/$', auth_views.password_change, name = 'change_password', kwargs={"post_change_redirect": 'achievements'}),
     url(r'^signed_up/$', direct_to_template, name='will_notify', kwargs={'template': 'emailauth/will_notify.html'}),
