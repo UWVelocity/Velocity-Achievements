45d1678ad4256b7f0f541da90cbab4da6573564b
Christophe Biocca <christophe.biocca@gmail.com>
2012-01-15 21:17:27 -0500

    Made the nomination page person specific.

--- velocity_achievements/achievements/views.py
+++ velocity_achievements/achievements/views.py
@@ -2,9 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Count
 from django import forms
 from django.forms import ModelForm
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.template import RequestContext
-from models import Participant, Nomination
+from models import Participant, Nomination, Achievement
 
 def achievements(request):
     participants = Participant.objects.annotate(num_grants=Count('grant')).order_by('-num_grants')
@@ -23,17 +23,41 @@ class NominateForm(ModelForm):
                 }
 
 @login_required
-def nominate(request, participant_id = None):
+def nominate(request):
     nomination = Nomination(nominator = request.user)
-    initial = {}
-    participant = Participant.objects.filter(pk = participant_id)
-    if participant:
-        initial['participant'] = participant.get()
     if request.method == 'POST':
-        form = NominateForm(request.POST, instance = nomination, initial = initial)
+        form = NominateForm(request.POST, instance = nomination)
         if form.is_valid():
             nomination = form.save()
             return redirect(achievements)
     else:
-        form = NominateForm(instance = nomination, initial = initial)
+        form = NominateForm(instance = nomination)
     return render_to_response('nominate.html', {'form': form}, context_instance=RequestContext(request))
+
+class NominatePersonForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(NominatePersonForm, self).__init__(*args, **kwargs)
+        self.fields['achievement'] = forms.ModelChoiceField(\
+                queryset = Achievement.objects.exclude(\
+                    grant__participant = self.instance.participant).exclude(\
+                    nomination__nominator = self.instance.nominator, \
+                    nomination__participant = self.instance.participant))
+
+    class Meta:
+        model = Nomination
+        exclude = ('nominator', 'participant')
+        widgets = {
+                'achievement': forms.RadioSelect()
+                }
+
+@login_required
+def nominate_person(request, participant_id):
+    nomination = Nomination(nominator = request.user, participant = get_object_or_404(Participant, pk=participant_id))
+    if request.method == 'POST':
+        form = NominatePersonForm(request.POST, instance = nomination)
+        if form.is_valid():
+            nomination = form.save()
+            return redirect(achievements)
+    else:
+        form = NominatePersonForm(instance = nomination)
+    return render_to_response('nominate_person.html', {'form': form, 'participant_id': participant_id}, context_instance=RequestContext(request))
